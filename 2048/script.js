9d79b51f81c74d9f892f2cfadfb5b8ec409aa8b3
frank921 <frankwang200209@gmail.com>
2022-05-06 13:00:56 +0200

    Basic finish of 2048 Game

--- 2048/script.js
+++ 2048/script.js
@@ -1,66 +1,324 @@
 "use strict";
 
 /* initial chess board */
-let line0 = [0,0,0,0];
-let line1 = [0,0,0,0];
-let line2 = [0,0,0,0];
-let line3 = [0,0,0,0];
+let chessboard = new Array([0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]);
 
 /* initial score */
 let score = 0;
+let isWin = false;
+let isLose = false;
 
-display();
+
+
+
+    
 
 function display(){
-    /* display chess board */
+    /* display and update the chess board */
     for(let i = 0; i < 4; i++){
         for(let j = 0; j < 4; j++){
-            if(i==0){
-                /* change value */
-                document.getElementById("g0"+"-"+j).innerHTML = line0[j];
-                /* change color */
-                document.getElementById("g0"+"-"+j).style.backgroundColor = `rgb(255,${210-200*(Math.log(line0[j])/8)},${210-200*(Math.log(line0[j])/8)})`;
-                if(line0[j] == 0){
-                  document.getElementById("g0"+"-"+j).innerHTML = "";
-                  document.getElementById("g0"+"-"+j).style.backgroundColor = "#b6acac82";
-                }
-
-            }else if(i==1){
-                /* change value */
-                document.getElementById("g1"+"-"+j).innerHTML = line1[j];
-                /* change color */
-                document.getElementById("g1"+"-"+j).style.backgroundColor = `rgb(255,${210-200*(Math.log(line1[j])/8)},${210-200*(Math.log(line1[j])/8)})`;
-                if(line1[j] == 0){
-                    document.getElementById("g1"+"-"+j).innerHTML = "";
-                    document.getElementById("g1"+"-"+j).style.backgroundColor = "#b6acac82";
-                }
-            }else if(i==2){
-                /* change value */
-                document.getElementById("g2"+"-"+j).innerHTML = line2[j];
-                /* change color */
-                document.getElementById("g2"+"-"+j).style.backgroundColor = `rgb(255,${210-200*(Math.log(line2[j])/8)},${210-200*(Math.log(line2[j])/8)})`;
-                if(line2[j] == 0){
-                    document.getElementById("g2"+"-"+j).innerHTML = "";
-                    document.getElementById("g2"+"-"+j).style.backgroundColor = "#b6acac82";
-                }
-            }else if(i==3){
-                /* change value */
-                document.getElementById("g3"+"-"+j).innerHTML = line3[j];
-                /* change color */
-                document.getElementById("g3"+"-"+j).style.backgroundColor = `rgb(255,${210-200*(Math.log(line3[j])/8)},${210-200*(Math.log(line3[j])/8)})`;
-                if(line3[j] == 0){
-                    document.getElementById("g3"+"-"+j).innerHTML = "";
-                    document.getElementById("g3"+"-"+j).style.backgroundColor = "#b6acac82";
-                }
-
+            
+            /* change value */
+            document.getElementById("g"+i+"-"+j).innerHTML = chessboard[i][j];
+            /* change color */
+            document.getElementById("g"+i+"-"+j).style.backgroundColor = `rgb(255,${210-200*(Math.log(chessboard[i][j])/8)},${210-200*(Math.log(chessboard[i][j])/8)})`;
+            if(chessboard[i][j] == 0){
+                document.getElementById("g"+i+"-"+j).innerHTML = "";
+                document.getElementById("g"+i+"-"+j).style.backgroundColor = "#b6acac82";
             }
 
-
-
-
+           
         }
+    }
+    /* display score */
+    document.getElementById("score").innerHTML = score;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //不含最大值，含最小值
+  }
+
+
+/* add random number to random grids*/
+function add(){
+    let is_full = false;
+    while(!is_full){
+
+        let x = getRandomInt(0,4);
+        let y = getRandomInt(0,4);
+        let value = getRandomInt(1,3);
+        
+        if(chessboard[y][x] == 0){
+            chessboard[y][x] = 2**value;
+            break;
+        }
+        /* change up to here */
+        is_full = true;
+        for(let i = 0; i < 4; i++){
+            for(let j = 0; j < 4; j++){
+                if(chessboard[i][j] == 0){
+                    is_full = false;
+                    break;
+                }
+            }
+        }
+        
+    }
+    if(is_full){
+        isLose = true;
+        alert("You Lose!");
+    }
+}
+
+
+function moveLeft(){
+    /* move all the grids to the left and merge if needed */
+    let is_moved = false;
+    let is_merged = new Array([false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]);
+    for(let i = 0; i < 4; i++){
+        for(let j=0; j < 4; j++){
+            if(chessboard[i][j] != 0){
+                let k = j;
+                while(k > 0){
+                    if(chessboard[i][k-1] == 0){
+                        chessboard[i][k-1] = chessboard[i][k];
+                        chessboard[i][k] = 0;
+                        is_moved = true;
+                        k--;
+                    }
+                    else if(chessboard[i][k] == chessboard[i][k-1] && !is_merged[i][k]){
+                        chessboard[i][k-1] = chessboard[i][k]*2;
+                        score += chessboard[i][k];
+                        is_merged[i][k] = true;
+                        chessboard[i][k] = 0;
+                        is_moved = true;
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(is_moved){
+        add();
+    }
+}
+
+
+function moveRight(){
+    /* move all the grids to the right and merge if needed */
+    let is_moved = false;
+    let is_merged = new Array([false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]);
+    for(let i = 0; i < 4; i++){
+        for(let j=3; j >= 0; j--){
+            if(chessboard[i][j] != 0){
+                let k = j;
+                while(k < 3){
+                    if(chessboard[i][k+1] == 0){
+                        chessboard[i][k+1] = chessboard[i][k];
+                        chessboard[i][k] = 0;
+                        is_moved = true;
+                        k++;
+                    }
+                    else if(chessboard[i][k] == chessboard[i][k+1] && !is_merged[i][k]){
+                        chessboard[i][k+1] = chessboard[i][k]*2;
+                        score += chessboard[i][k];
+                        is_merged[i][k] = true;
+                        chessboard[i][k] = 0;
+                        is_moved = true;
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(is_moved){
+        add();
     }
 }
 
 
 
+function moveUp(){
+    /* move all the grids to the up and merge if needed */
+    let is_moved = false;
+    let is_merged = new Array([false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]);
+    for(let i = 0; i < 4; i++){
+        for(let j=0; j < 4; j++){
+            if(chessboard[j][i] != 0){
+                let k = j;
+                while(k > 0){
+                    if(chessboard[k-1][i] == 0){
+                        chessboard[k-1][i] = chessboard[k][i];
+                        chessboard[k][i] = 0;
+                        k--;
+                        is_moved = true;
+                    }
+                    else if(chessboard[k][i] == chessboard[k-1][i] && !is_merged[k][i]){
+                        chessboard[k-1][i] = chessboard[k][i]*2;
+                        score += chessboard[k][i];
+                        is_merged[k][i] = true;
+                        chessboard[k][i] = 0;
+                        is_moved = true;
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(is_moved){
+        add();
+    }
+}
+
+function moveDown(){
+    /* move all the grids to the down and merge if needed */
+    let is_moved = false;
+    let is_merged = new Array([false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]);
+    for(let i = 0; i < 4; i++){
+        for(let j=3; j >= 0; j--){
+            if(chessboard[j][i] != 0){
+                let k = j;
+                while(k < 3){
+                    if(chessboard[k+1][i] == 0){
+                        chessboard[k+1][i] = chessboard[k][i];
+                        chessboard[k][i] = 0;
+                        k++;
+                        is_moved = true;
+                    }
+                    else if(chessboard[k][i] == chessboard[k+1][i] && !is_merged[k][i]){
+                        chessboard[k+1][i] = chessboard[k][i]*2;
+                        score += chessboard[k][i];
+                        is_merged[k][i] = true;
+                        chessboard[k][i] = 0;
+                        is_moved = true;
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(is_moved){
+        add();
+    }
+}
+
+function newGame(){
+    /* start a new game */
+    score = 0;
+    isLose = false;
+    isWin = false;
+    chessboard = new Array([0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]);
+    add();
+    display();
+}
+
+
+
+
+/* move the chess board */
+document.onkeydown = move;
+
+
+function move(event){
+    let e=window.event||event;
+    if(check_lose()){
+        isLose = true;
+        alert("You Lose!");
+    }
+    if(check_win()){
+        isWin = true;
+        alert("You Win!");
+    }  
+    switch(e.keyCode){
+        case 37: //left;
+            moveLeft();
+            display();
+            break;
+        
+        case 38: //up;
+            moveUp();
+            display();
+            break;
+        
+        case 39: //right;
+
+            moveRight();
+            display();
+            break;
+        
+        case 40: //down;
+            moveDown();
+            display();
+            break;
+    }
+    
+        
+    
+}
+
+function check_lose(){
+    /* check if the game is lose */
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            if(chessboard[i][j] == 0){
+                return false;
+            }
+        }
+    }
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 3; j++){
+            if(chessboard[i][j] == chessboard[i][j+1]){
+                return false;
+            }
+    
+        }
+    }
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 4; j++){
+            if(chessboard[i][j] == chessboard[i+1][j]){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function check_win(){
+    /* check if the game win */
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            if(chessboard[i][j] == 2048){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function cheat(){
+    /* cheat */
+    chessboard[0][0] = 2048;
+    display();
+    if(check_win()){
+        isWin = true;
+        
+        alert("You Win!");
+    }  
+}
+    
+newGame();
